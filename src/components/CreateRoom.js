48a7a38f7305923a16389b8d9c4d6d897e7d4a90
bang813 <bang97@ucla.edu>
2020-02-19 02:07:24 -0800

    add createRoom page

--- src/components/CreateRoom.js
+++ src/components/CreateRoom.js
@@ -1,11 +1,42 @@
 import React, { Component } from 'react'
+import Draggable from 'react-draggable';
+import {Whiteboard, EventStream, EventStore} from '@ohtomi/react-whiteboard';
 
-export default class CreateRoom extends Component {
+export default class CreateRoom extends Component {  
   render() {
+    const { name, room } = this.props.match.params;
     return (
       <div>
-        <h1>type number to create room</h1>
+        <Draggable
+          axis="both"
+          handle=".handle"
+          defaultPosition={{x: 0, y: 0}}
+          position={null}
+          grid={[25, 25]}
+          scale={1}>
+          <div className="handle" style={{backgroundColor: 'lightgreen', width: '30%', cursor: 'grab'}}>
+            Drag from here
+            <h1>name: {name}, room: {room}</h1>
+          </div>
+        </Draggable>
+        <Draggable
+          axis="both"
+          handle=".handle"
+          defaultPosition={{x: 0, y: 0}}
+          position={null}
+          grid={[25, 25]}
+          scale={1}>
+          <div>
+            <div className="handle" style={{backgroundColor: 'lightgreen', width: '30%', cursor: 'grab'}}>
+              Drag from here
+            </div>
+            <Whiteboard
+              events={new EventStream()} eventStore={new EventStore()}
+              style={{backgroundColor: 'lightyellow'}}
+            />
+          </div>
+        </Draggable>
       </div>
-    )
+    );
   }
 }
