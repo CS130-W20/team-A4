fe28a85fdbe33ff796522b3da112100d33bb42b9
heboyuan <heboyuan@DESKTOP-QTJA8ET.localdomain>
2020-02-19 22:49:22 -0800

    create room

--- src/backend/app.js
+++ src/backend/app.js
@@ -5,8 +5,16 @@ var uuidv4 = require('uuid/v4')
 var { Client } = require('pg')
 var client = new Client()
 
+var create_text_table = "CREATE TABLE text_table (\
+                            component_id INT PRIMARY KEY, \
+                            room_id VARCHAR NOT NULL\
+                            location BOX NOT NULL, \
+                            content TEXT \
+                        );"
+
 await client.connect()
-await client.query("CREATE TABLE ")
+await client.query(create_text_table)
+
 server.listen(80)
 
 var index_path = '/home/ubuntu/team-A4/public/index.html'
@@ -18,8 +26,8 @@ app.get('/', function (req, res) {
 app.post('/create_room', function (req, res) {
     current_uuid = uuidv4()
     res.send({uuid: current_uuid})
-    client.query('')
 })
+
 
 io.on('connection', function (socket) {
     socket.emit('news', { hello: 'world' })
