2b394670f776704681fe7a0253475780b59490cb
Feilan Wang <wangfeilan@hotmail.com>
2020-02-19 18:05:50 -0800

    resizable

--- src/components/CreateRoom.js
+++ src/components/CreateRoom.js
@@ -1,41 +1,70 @@
 import React, { Component } from 'react'
 import Draggable from 'react-draggable';
 import {Whiteboard, EventStream, EventStore} from '@ohtomi/react-whiteboard';
+import { Rnd } from "react-rnd";
 
-export default class CreateRoom extends Component {  
+const style = {
+  display: "flex",
+  alignItems: "center",
+  justifyContent: "center",
+  border: "solid 1px #ddd",
+  background: "#f0f0f0"
+};
+
+export default class CreateRoom extends Component {
   render() {
     const { name, room } = this.props.match.params;
     return (
       <div>
-        <Draggable
-          axis="both"
-          handle=".handle"
-          defaultPosition={{x: 0, y: 0}}
-          position={null}
-          grid={[25, 25]}
-          scale={1}>
-          <div className="handle" style={{backgroundColor: 'lightgreen', width: '30%', cursor: 'grab'}}>
-            Drag from here
-            <h1>name: {name}, room: {room}</h1>
-          </div>
-        </Draggable>
-        <Draggable
-          axis="both"
-          handle=".handle"
-          defaultPosition={{x: 0, y: 0}}
-          position={null}
-          grid={[25, 25]}
-          scale={1}>
-          <div>
+        <Rnd
+        style={style}
+        default={{
+          x: 0,
+          y: 0,
+          width: 320,
+          height: 200,
+        }}
+        >
+          <Draggable
+            axis="both"
+            handle=".handle"
+            defaultPosition={{x: 0, y: 0}}
+            position={null}
+            grid={[25, 25]}
+            scale={1}>
             <div className="handle" style={{backgroundColor: 'lightgreen', width: '30%', cursor: 'grab'}}>
               Drag from here
+              <h1>name: {name}, room: {room}</h1>
             </div>
-            <Whiteboard
-              events={new EventStream()} eventStore={new EventStore()}
-              style={{backgroundColor: 'lightyellow'}}
-            />
-          </div>
-        </Draggable>
+          </Draggable>
+        </Rnd>
+        <Rnd
+        style={style}
+        default={{
+          x: 0,
+          y: 0,
+          width: 320,
+          height: 200,
+        }}
+        >
+          <Draggable
+            axis="both"
+            handle=".handle"
+            defaultPosition={{x: 0, y: 0}}
+            position={null}
+            grid={[25, 25]}
+            scale={1}>
+            <div>
+              <div className="handle" style={{backgroundColor: 'lightgreen', width: '30%', cursor: 'grab'}}>
+                Drag from here
+              </div>
+              <Whiteboard
+                events={new EventStream()} eventStore={new EventStore()}
+                style={{backgroundColor: 'lightyellow'}}
+              />
+            </div>
+          </Draggable>
+        </Rnd>
       </div>
     );
   }
