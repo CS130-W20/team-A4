92e7a97c87392d7c0b75ec40fe6f23f21923b89b
Ubuntu <ubuntu@ip-172-31-37-58.us-west-2.compute.internal>
2020-02-20 08:32:16 +0000

    create text component

--- src/backend/app.js
+++ src/backend/app.js
@@ -3,21 +3,29 @@ var server = require('http').Server(app)
 var io = require('socket.io')(server)
 var uuidv4 = require('uuid/v4')
 var { Client } = require('pg')
-var client = new Client()
 
-var create_text_table = "CREATE TABLE text_table (\
-                            component_id INT PRIMARY KEY, \
-                            room_id VARCHAR NOT NULL\
-                            location BOX NOT NULL, \
-                            content TEXT \
-                        );"
+db_config = {
+	user: 'dbuser',
+	password: '1234',
+	database: 'devdb'
+}
 
-await client.connect()
-await client.query(create_text_table)
+var create_text_table = "CREATE TABLE IF NOT EXISTS text_table (\
+                          component_id VARCHAR PRIMARY KEY, \
+                          room_id VARCHAR NOT NULL, \
+                          location BOX NOT NULL, \
+                          content TEXT \
+						);"
+						
+var create_text_component = "INSERT INTO text_table VALUES($1, $2, '((0, 0), (100, 100))', 'Enter text here');"
 
-server.listen(80)
+
+var client = new Client(db_config)
+client.connect()
+client.query(create_text_table)
 
 var index_path = '/home/ubuntu/team-A4/public/index.html'
+server.listen(80)
 
 app.get('/', function (req, res) {
     res.sendFile(index_path)
@@ -28,6 +36,21 @@ app.post('/create_room', function (req, res) {
     res.send({uuid: current_uuid})
 })
 
+app.post('/create_component', function (req, res) {
+	current_component_id = uuidv4()
+	// console.log(req.query.component_type)
+	// console.log("text")
+	// console.log(req.query.component_type == "text")
+	switch(req.query.component_type){
+		case "text":
+			client.query(create_text_component, [current_component_id, req.query.room_id])
+			res.send({component_id: current_component_id})
+			break;
+		default:
+			console.error("ERROR: Unrecongized component type")
+			res.send("Please send valid request")
+	}
+})
 
 io.on('connection', function (socket) {
     socket.emit('news', { hello: 'world' })
