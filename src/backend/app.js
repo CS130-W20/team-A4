6665e40d103f4c095b29f8c432730b29f191cbcf
heboyuan <boyuan_heboyuan@live.com>
2020-02-19 22:23:41 -0800

    sync

--- src/backend/app.js
+++ src/backend/app.js
@@ -1,18 +1,29 @@
-var app = require('express')();
-var server = require('http').Server(app);
-var io = require('socket.io')(server);
+var app = require('express')()
+var server = require('http').Server(app)
+var io = require('socket.io')(server)
+var uuidv4 = require('uuid/v4')
+var { Client } = require('pg')
+var client = new Client()
 
-server.listen(80);
+await client.connect()
+await client.query("CREATE TABLE ")
+server.listen(80)
 
 var index_path = '/home/ubuntu/team-A4/public/index.html'
 
 app.get('/', function (req, res) {
-  res.sendFile(index_path);
-});
+    res.sendFile(index_path)
+})
+
+app.post('/create_room', function (req, res) {
+    current_uuid = uuidv4()
+    res.send({uuid: current_uuid})
+    client.query('')
+})
 
 io.on('connection', function (socket) {
-  socket.emit('news', { hello: 'world' });
-  socket.on('my other event', function (data) {
-    console.log(data);
-  });
-});
+    socket.emit('news', { hello: 'world' })
+    socket.on('my other event', function (data) {
+        console.log(data)
+    })
+})
