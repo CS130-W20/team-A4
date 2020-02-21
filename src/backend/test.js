8d4789c1a184affba3b7b75232a29a4ff03d8c4f
461 <stevennm33@gmail.com>
2020-02-21 00:34:34 -0800

    add test_cases

--- src/backend/test.js
+++ src/backend/test.js
@@ -1,1 +1,12 @@
 import io from "socket.io-client";
+
+//Connection
+const socket = io( "ec2-54-184-200-244.us-west-2.compute.amazonaws.com:8080", {
+    "transports": ["polling","websocket"]
+});
+
+var test_case = process.argv
+
+switch(test_case){
+    case create_room
+}
