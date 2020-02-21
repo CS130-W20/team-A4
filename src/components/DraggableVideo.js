4c15607918ea2a4bbb180751b013764117f4db70
bang813 <bang97@ucla.edu>
2020-02-20 23:48:17 -0800

    add import

--- src/components/DraggableVideo.js
+++ src/components/DraggableVideo.js
@@ -4,6 +4,7 @@ import Iframe from 'react-iframe';
 import style from "../assets/jss/draggableStyle";
 import Card from '@material-ui/core/Card';
 import CardHeader from '@material-ui/core/CardHeader';
+import CardActions from '@material-ui/core/CardActions';
 import CardContent from '@material-ui/core/CardContent';
 import TextField from '@material-ui/core/TextField';
 import Button from '@material-ui/core/Button';
