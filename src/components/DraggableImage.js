bfd4cb554224948131e953490062f04ff054189b
bang813 <bang97@ucla.edu>
2020-03-06 16:05:44 -0800

    change rnd, add move button

--- src/components/DraggableImage.js
+++ src/components/DraggableImage.js
@@ -2,9 +2,10 @@ import React, { Component } from 'react'
 import { Rnd } from "react-rnd";
 import Card from '@material-ui/core/Card';
 import CardActions from '@material-ui/core/CardActions';
+import CardContent from '@material-ui/core/CardContent';
 import IconButton from '@material-ui/core/IconButton';
 import DeleteIcon from '@material-ui/icons/Delete';
-
+import OpenWithIcon from '@material-ui/icons/OpenWith';
 import style from "../assets/jss/draggableStyle";
 
 export default class DraggableImage extends Component {
@@ -38,26 +39,30 @@ export default class DraggableImage extends Component {
           height: 400,
         }}
         enableUserSelectHack={false}
+        dragHandleClassName="moveable"
       >
         <Card style={{ width: '100%', height: '100%' }} >
-        <CardActions>
-          <IconButton aria-label="delete" onClick={() => this.props.handleDeleteComponent(this.props.k)} >
-            <DeleteIcon fontSize="small" />
-          </IconButton>
-        </CardActions>
-        {show ?
-            <img 
-                src={this.state.selectedImage} 
-                width="90%" 
-                height="80%"
-                alt="image cannot be displayed"
-            /> 
-        :
-            <input type="file" onChange={this.imageSelectedHandler}/>
-        }
+          <CardActions>
+            <IconButton aria-label="delete" onClick={() => this.props.handleDeleteComponent(this.props.k)} >
+              <DeleteIcon fontSize="small" />
+            </IconButton>
+            <IconButton style={{ marginLeft: 'auto', cursor: 'all-scroll' }} aria-label="move" className="moveable">
+              <OpenWithIcon />
+            </IconButton>
+          </CardActions>
+          <CardContent>
+            {show ?
+              <img 
+                  src={this.state.selectedImage} 
+                  width="90%" 
+                  height="80%"
+                  alt="image cannot be displayed"
+              /> 
+            :
+              <input type="file" onChange={this.imageSelectedHandler}/>
+            }
+            </CardContent>
         </Card>
-
-
       </Rnd>
     )
   }
