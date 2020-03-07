2404ef88f16d538428ada3efab6ea2d6a4dc9a9e
TT-F <stevennm33@gmail.com>
2020-03-06 16:24:07 -0800

    "small update"

--- src/components/SocketContext.js
+++ src/components/SocketContext.js
@@ -1,9 +1,5 @@
 import io from 'socket.io-client'
 
-<<<<<<< HEAD
 const socket = io ("http://ec2-50-112-33-65.us-west-2.compute.amazonaws.com:8084", {"transports": ["polling","websocket"]});
-=======
-const socket = io ("ec2-50-112-33-65.us-west-2.compute.amazonaws.com:8084", {"transports": ["polling","websocket"]});
->>>>>>> 11e308c0727fcc8bfe51a38653add43296d636e3
 
 export default socket;
